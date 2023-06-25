32b66c94332a2472705658224bc7c17c6508c27a
ffflukentp <Natthaphonst52@gmail.com>
2023-06-25 11:10:35 +0700

    Update Introduction

---
+++ lib/views/home_ui.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class HomeUI extends StatefulWidget {
+  const HomeUI({super.key});
+
+  @override
+  State<HomeUI> createState() => _HomeUIState();
+}
+
+class _HomeUIState extends State<HomeUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepOrange[100],
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange,
+        title: Text(
+          'คำนวณค่างวดรถ',
+        ),
+        centerTitle: true,
+      ),
+    );
+  }
+}
